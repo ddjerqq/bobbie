be5ebf565e713186de5aef3d9093260e6775490f
Révaz <revazed@outlook.com>
2022-01-19 12:12:18 +0100

    add a command to be able to talk through a bot in a specific channel

--- app.py
+++ app.py
@@ -18,6 +18,14 @@ class App(discord.Client):
             print("Something went wrong:", e)
             exit()
 
+    async def on_message(self, message):
+        if message.content.startswith("!say"):
+            content = message.content
+            process_message = str(content).replace("!say", "").strip()
+            channel = app.get_channel(740486454240870420)
+            await channel.send(process_message)
+
+
 
 app = App(intents=intents)
 app.run("OTMzMjQzODQwOTA1NzY5MDQw.YeetDg.--AR1OpWo1NZkLz-jzC0PaOuJmI")
