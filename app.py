46f8db3c25e24b73fba961790dba8684b00af631
Révaz <revazed@outlook.com>
2022-01-19 09:36:24 +0100

    implement status, activity

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ import requests as r
 intents = discord.Intents.default()
 intents.members = True
 
+
 class App(discord.Client):
     async def on_ready(self):
         print("Connected:", self.user)
+        try:
+            game = discord.Game("discord.gg/georgia")
+            status = discord.Status.dnd
+            await app.change_presence(status=status, activity=game)
+        except Exception as e:
+            print("Something went wrong:", e)
+            exit()
+
 
 app = App(intents=intents)
-app.run(token="OTMzMjQzODQwOTA1NzY5MDQw.YeetDg.--AR1OpWo1NZkLz-jzC0PaOuJmI")
+app.run("OTMzMjQzODQwOTA1NzY5MDQw.YeetDg.--AR1OpWo1NZkLz-jzC0PaOuJmI")
