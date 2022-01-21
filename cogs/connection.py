560e3b678605cf694a688b0053f092eeea83186e
Révaz <revazed@outlook.com>
2022-01-21 11:55:48 +0100

    implement discord activity and status

--- cogs/connection.py
+++ cogs/connection.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+import app
+
 
 class Connection(commands.Cog):
 
@@ -10,6 +12,9 @@ class Connection(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Claude succesfully connected.")
+        activity = discord.Game("discord.gg/Georgia")
+        status = discord.Status.dnd
+        await app.app.change_presence(activity=activity, status=status)
 
 
 def setup(app):
