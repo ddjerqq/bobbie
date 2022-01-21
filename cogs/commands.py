dead4597b034e0094b192cf34541cbea27ef6b50
Révaz <revazed@outlook.com>
2022-01-21 10:08:09 +0100

    add !ping command

--- cogs/commands.py
+++ cogs/commands.py
@@ -7,6 +7,10 @@ class Commands(commands.Cog):
     def __init(self, app):
         self.app = app
 
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong")
+
 
 def setup(app):
     app.add_cog(Commands(app))
