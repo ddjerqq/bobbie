4f595bce59c37d4d954eb517fd9cc8d61443a5b1
Révaz <revazed@outlook.com>
2022-01-24 14:04:26 +0100

    rewrite weather function

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,4 +1,6 @@
 import discord
+import json
+import requests as r
 from discord.ext import commands
 
 
@@ -9,16 +11,11 @@ class Utility(commands.Cog):
 
     @commands.command()
     async def weather(self, ctx, arg):
-        geolocate = Nominatim(user_agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) "
-                                         "Chrome/41.0.2228.0 Safari/537.36")
-        weather_api = "https://api.darksky.net/forecast/abe6a84811a8ab8f1f39cd9b8b8f40e1/{},{}"
-        location = geolocate.geocode(arg)
-        process = r.get(weather_api.format(location.latitude, location.longitude))
-        with open("cogs/weather.json", "w") as w:
-            json.dump(process.json(), w, indent=4)
-        with open("cogs/weather.json", "r+") as weather:
-            weather_data = json.load(weather)
-        await ctx.send("weather in %s: %s" % (arg, weather_data["currently"]["summary"]))
+        key = "10d195e2163c3b8d5b62bfeec7f45da0"
+        data = r.get("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=67693c9353acbcb58d6ace5e51825990/" % arg)
+        with open("cogs/weather.json", "w+") as f:
+            json.dump(data.json(), f, indent=4)
+
 
     @commands.command()
     async def avatar(self, ctx, *, avamember: discord.Member = None):
