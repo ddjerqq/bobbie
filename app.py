d6bc1d8160a66d8013ef6f6695997bdba19a5933
Révaz <revazed@outlook.com>
2022-01-24 12:33:49 +0100

    revert changes

--- app.py
+++ app.py
@@ -26,11 +26,6 @@ for filename in os.listdir("./cogs"):
         app.load_extension(f"cogs.{filename[:-3]}")
 
 
-@app.listen("on_message")
-async def neko(message):
-    emoji = app.get_emoji(935132922669842452)
-    if message.content.startswith("neko" or "Neko"):
-        await message.channel.send("https://cdn.discordapp.com/emojis/451544987952218112.gif?size=96&quality=lossless")
 
 
 app.run("OTMzMjQzODQwOTA1NzY5MDQw.YeetDg.b4icjZ7hgbXTtTn_4GR-QWYKyuw")
