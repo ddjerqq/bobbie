cdfcb777871f6b4660557375f3bc749687e81274
Révaz <revazed@outlook.com>
2022-01-24 09:43:08 +0100

    update discord status

--- cogs/connection.py
+++ cogs/connection.py
@@ -12,7 +12,7 @@ class Connection(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Bobbie succesfully connected.")
-        activity = discord.Game("Bobbie's Campfire!")
+        activity = discord.Game("discord.gg/georgia")
         status = discord.Status.dnd
         await app.app.change_presence(activity=activity, status=status)
 
