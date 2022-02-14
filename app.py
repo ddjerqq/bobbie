6296f57794c69d6b949a6da75e8c54f895920e55
Révaz <revazed@outlook.com>
2022-02-14 16:40:07 +0100

    add message deletion, member leaving logging

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import requests as r
 
 intents = discord.Intents.default()
 intents.members = True
-app = commands.Bot(command_prefix="!", help_command=None)
+app = commands.Bot(command_prefix="!", help_command=None, intents=intents)
 
 
 @app.command()
@@ -24,6 +24,31 @@ async def unload(ctx, extension):
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         app.load_extension(f"cogs.{filename[:-3]}")
+
+
+@app.listen()
+async def on_message_delete(message):
+    channel = app.get_channel(939534645798793247)
+    msg_content = message.content
+    msg_author = message.author
+    author_id = message.author.id
+    embed = discord.Embed(title="Bobbie's Archives", description="ბობიმ შეამჩნია წაშლილი მესიჯი მაგრამ მოასწრო "
+                                                                 "სქრინის გადაღება!", color=0x2d56a9)
+    embed.add_field(name="ავტორი", value="%s" % msg_author, inline=True)
+    embed.add_field(name="ID", value="%s" % author_id, inline=True)
+    embed.add_field(name="მესიჯი", value="%s" % msg_content, inline=False)
+    if author_id == 933243840905769040:
+        pass
+    else:
+        await channel.send(embed=embed)
+
+
+@app.listen()
+async def on_member_remove(member):
+    member_name = member.name
+    member_id = member.id
+    channel = app.get_channel(942800528822370315)
+    await channel.send("გავიდა დისქორდიდან: %s, ID: %s" % (member_name, member_id))
 
 
 app.run("OTMzMjQzODQwOTA1NzY5MDQw.YeetDg.0VU1hSrwSyGC96VVygS82L38zck")
