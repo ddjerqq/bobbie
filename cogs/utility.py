beb74ccb0f3a3b1966da8cffe14fac4949818835
Révaz <revazed@outlook.com>
2022-02-22 23:36:42 +0100

    groundwork for !userinfo command

--- cogs/utility.py
+++ cogs/utility.py
@@ -33,6 +33,28 @@ class Utility(commands.Cog):
         process = content.replace("!say", "").strip()
         await channel.send(process)
 
+    @commands.command()
+    async def userinfo(self, ctx, target: discord.Member = None):
+        try:
+            if target is None:
+                target = ctx.message.author
+                embed = discord.Embed(color=0x2d56a9)
+                embed.add_field(name="%s" % target, value="ID: %s" % target.id, inline=False)
+                embed.add_field(name="დაჯოინდა", value="%s-%s-%s" % (target.joined_at.day, target.joined_at.month, target.joined_at.year), inline=True)
+                embed.add_field(name="რეგისტრაცია", value="%s-%s-%s" % (target.created_at.day, target.created_at.month, target.created_at.year), inline=True)
+                embed.set_thumbnail(url=target.avatar_url)
+                await ctx.send(embed=embed)
+            else:
+                roles = "".join(target.roles)
+                embed = discord.Embed(color=0x2d56a9)
+                embed.add_field(name="%s" % target.name, value="ID: %s" % target.name, inline=False)
+                embed.add_field(name="დაჯოინდა", value="%s-%s-%s" % (target.joined_at.day, target.joined_at.month, target.joined_at.year), inline=True)
+                embed.add_field(name="რეგისტრაცია", value="%s-%s-%s" % (target.created_at.day, target.created_at.month, target.created_at.year), inline=True)
+                embed.set_thumbnail(url=target.avatar_url)
+                await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(e)
+
 
 def setup(app):
     app.add_cog(Utility(app))
