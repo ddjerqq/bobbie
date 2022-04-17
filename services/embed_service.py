1161c098cb2ffb547de1b4cc75b8474bf8df651a
Révaz <revazed@outlook.com>
2022-04-17 19:13:20 +0200

    change hunt, fish text

--- services/embed_service.py
+++ services/embed_service.py
@@ -140,7 +140,7 @@ class EmbedService:
         return em
 
     def fish(self, item: Item, broken: bool) -> disnake.Embed:
-        em = disnake.Embed(description=f"შენ დაიჭირე {item.type} {EMOJIS['fishing_rod']}",
+        em = disnake.Embed(description=f"შენ წახვედი სათევზაოდ და დაიჭირე {item.type} {EMOJIS['fishing_rod']}",
                            color=0x00ff00 if not broken else 0xff0000)
         em.description += "\nშენ გატეხე შენი ანკესი" if broken else ""
         em.add_field(name="ღირებულება",
@@ -151,7 +151,7 @@ class EmbedService:
         return em
 
     def hunt(self, item: Item, broken: bool) -> disnake.Embed:
-        em = disnake.Embed(description=f"{item.type} {EMOJIS['hunting_rifle']}",
+        em = disnake.Embed(description=f"შენ წახვედი სანადიროდ და მოინადირე {item.type} {EMOJIS['hunting_rifle']}",
                            color=0x00ff00 if not broken else 0xff0000)
         em.description += "\nშენ გატეხე შენი სანადირო თოფი" if broken else ""
         em.add_field(name="ღირებულება",
