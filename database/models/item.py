29653e64c69fa0b60d916c119a896992455338f0
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-12 17:34:44 +0400

    tweaked item.py

--- database/models/item.py
+++ database/models/item.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from database.id import Id
 from database.config import *
+from database.rarity import Rarity
 
 
 class Item:
@@ -20,19 +21,11 @@ class Item:
         (0.45; 1.00] => Battle-Scarred
     """
 
-    def __init__(self, id: int, type: ItemType, rarity: float, owner_id: int | None):
-        self.__broken = None  # type: bool | None
+    def __init__(self, id: int, type: ItemType, rarity: Rarity, owner_id: int | None):
         self.__id = id
         self.type = type
-        self.__rarity = rarity
+        self.rarity = rarity
         self.owner_id = owner_id
-
-    # TODO move this to factory
-    @classmethod
-    def new(cls, type: str) -> Item:
-        id = Id.new()
-        rarity = random.random() ** 2
-        return cls(id, type, rarity, None)
 
     @property
     def id(self) -> int:
@@ -47,7 +40,8 @@ class Item:
     @property
     def buyable(self):
         """
-        :return: bool if the item is in {"fishing_rod", "hunting_rifle", "shovel"}
+        check if the item is
+        {"fishing_rod", "hunting_rifle", "shovel", "knife"}
         """
         try:
             Buyable[self.type.name]
@@ -80,25 +74,6 @@ class Item:
         return ItemName[self.type.name].value
 
     @property
-    def rarity_string(self) -> str:
-        if   0.0 <= self.__rarity <= 0.07:
-            return "სულ ახალი"
-        elif 0.07 < self.__rarity <= 0.15:
-            return "მინიმალურად გამოყენებული"
-        elif 0.15 < self.__rarity <= 0.38:
-            return "ოდნავ გამოყენებული"
-        elif 0.38 < self.__rarity <= 0.45:
-            return "კარგად ნახმარი"
-        elif 0.45 < self.__rarity <= 1.00:
-            return "დაგლეჯილი"
-        else:
-            raise ValueError(f"Invalid rarity: {self.__rarity}")
-
-    @property
-    def rarity(self) -> float:
-        return self.__rarity
-
-    @property
     def created_at(self) -> datetime:
         """
         get the creation datetime of Item.
@@ -107,13 +82,6 @@ class Item:
         """
         return Id.created_at(self.__id)
 
-    @property
-    def will_break(self) -> bool:
-        """try break the item, return True if the item broke"""
-        if not self.__broken:
-            self.__broken = random.random() < self.__rarity ** 2.5
-        return self.__broken
-
     def __hash__(self):
         return hash(self.db)
 
@@ -121,39 +89,15 @@ class Item:
         return isinstance(other, Item) and self.__id == other.__id
 
     def __str__(self):
-        return f"{self.type.name} {self.rarity_string} owner: {self.owner_id}"
+        return f"{self.type.name} {self.rarity.name} owner: {self.owner_id}"
 
     def __repr__(self):
-        return f"<Item id={self.id} type={self.type} owner={self.owner_id} rarity={self.__rarity}>"
+        return f"<Item id={self.id} type={self.type.name} owner={self.owner_id} rarity={self.rarity!r}>"
 
     # TODO make this return a dict
     @property
     def db(self) -> tuple:
         return self.__id, self.type, self.__rarity, self.owner_id
-
-    # TODO move this to factory
-    @classmethod
-    def from_db_row(cls, data: sqlite3.Row) -> Item:
-        return cls(**dict(data))
-
-    def use(self) -> Item:
-        """
-        generate a random item from the given type of the tool.
-        :return: an item of the given type
-        """
-        match self.type:
-            case ItemType.FISHING_ROD:
-                group = FishableRandomWeight
-            case ItemType.HUNTING_RIFLE:
-                group = HuntableRandomWeight
-            case ItemType.SHOVEL:
-                group = DigableRandomWeight
-            case _:
-                raise ValueError(f"item type '{self.type}' is not a valid tool, must be one of {[*Buyable]}")
-
-        random_type = random.choice(list(iter(group)))
-
-        return self.new(random_type)
 
     # TODO factory pattern
     @classmethod
