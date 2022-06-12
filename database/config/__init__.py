0c9a2e5bbc743011d204a5db03e00e020f4707d8
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-12 16:28:42 +0400

    added item price enum.

--- database/config/__init__.py
+++ database/config/__init__.py
@@ -1,10 +1,11 @@
 from . emoji import *
 from . names import *
+from . prices import *
 from . random_chance_weights import *
 from . thumbnails import *
 from . tools import *
 
-__all__ = ["ItemEmoji", "ItemName", "ItemRandomWeight", "ItemThumbnail", "Buyable"]
+__all__ = ["ItemEmoji", "ItemName", "ItemPrice", "ItemRandomWeight", "ItemThumbnail", "Buyable"]
 
 
 
---
+++ database/config/prices/item_price.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class ItemPrice(Enum):
+    FISHING_ROD   = 15
+    COMMON_FISH   = 5
+    RARE_FISH     = 10
+    TROPICAL_FISH = 20
+    SHARK         = 40
+    GOLDEN_FISH   = 50
+    HUNTING_RIFLE = 20
+    PIG           = 5
+    DEER          = 10
+    BEAR          = 20
+    WOLF          = 30
+    TIGER         = 40
+    LION          = 50
+    ELEPHANT      = 60
+    SHOVEL        = 15
+    COPPER_COIN   = 1
+    EMERALD       = 10
+    RUBY          = 20
+    SAPPHIRE      = 30
+    AMETHYST      = 40
+    DIAMOND       = 50
+    KNIFE         = 50
