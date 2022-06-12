02a9088eb3a8b0849ccbd634938ee179ac02c069
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-13 01:19:34 +0400

    tweaked database :D

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,4 @@
 from database.database import Database
-# TODO models here
+from factories.item_factory import ItemFactory
+from factories.user_factory import UserFactory
+from config.item_types import ItemType
--- database/database.py
+++ database/database.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
-import random
-
 import aiosqlite
-
 from database.services.user_service import UserService
 
 
@@ -13,29 +10,8 @@ class Database:
         self._cursor     = cursor
         self.users       = UserService(connection, cursor)
 
-
-    async def verify_word(self, word: str, status: int):
-        """
-        verify word in database \n
-        :param str word:
-        :param int status: if valid 1, else 0
-        """
-        await self._cursor.execute("""
-        UPDATE words
-        SET verified=?
-        WHERE word=?
-        """, (status, word))
-
-    async def unverified_word(self):
-        await self._cursor.execute("""
-        SELECT * FROM words
-        WHERE verified IS NULL OR verified=0
-        """)
-        words = await self._cursor.fetchall()
-        return random.choice(list(words))
-
     @classmethod
-    async def ainit(cls, path: str):
-        connection  = await aiosqlite.connect(path)
-        cursor      = await connection.cursor()
+    async def ainit(cls, path: str) -> Database:
+        connection = await aiosqlite.connect(path)
+        cursor     = await connection.cursor()
         return cls(connection, cursor)
