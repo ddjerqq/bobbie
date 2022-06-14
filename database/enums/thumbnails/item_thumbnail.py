93b3722c801f62428351b146b178778c9844f791
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-14 14:41:12 +0400

    fixed thumbnail issue

--- database/enums/thumbnails/item_thumbnail.py
+++ database/enums/thumbnails/item_thumbnail.py
@@ -2,21 +2,21 @@ from enum import Enum
 
 
 class ItemThumbnail(Enum):
-    FISHING_ROD   = "https://i.imgur.com/m7HBPHl.png",
-    HUNTING_RIFLE = "https://i.imgur.com/pjtWTSg.png",
-    SHOVEL        = "https://i.imgur.com/Dod0FE4.png",
-    COMMON_FISH   = "https://i.imgur.com/I3jU3p7.png",
-    RARE_FISH     = "https://i.imgur.com/7f90E9p.png",
-    TROPICAL_FISH = "https://i.imgur.com/ZBVZRXw.png",
-    SHARK         = "https://i.imgur.com/NMeTrjK.png",
-    GOLDEN_FISH   = "https://i.imgur.com/o2m9RkM.png",
-    PIG           = "https://i.imgur.com/v1Qa101.png",
-    DEER          = "https://i.imgur.com/fGl3N7s.png",
-    WOLF          = "https://i.imgur.com/k0fRgHl.png",
-    BEAR          = "https://i.imgur.com/4CryBHi.png",
-    LION          = "https://i.imgur.com/FBOyDvA.png",
-    COPPER_COIN   = "https://i.imgur.com/BR73e8Q.png",
-    ELEPHANT      = "https://i.imgur.com/qchItpk.png",
-    RUBY          = "https://i.imgur.com/rqOuL5x.png",
-    SAPPHIRE      = "https://i.imgur.com/9zrHNnb.png",
-    AMETHYST      = "https://i.imgur.com/cookhhr.png",
+    FISHING_ROD   = "https://i.imgur.com/m7HBPHl.png"
+    HUNTING_RIFLE = "https://i.imgur.com/pjtWTSg.png"
+    SHOVEL        = "https://i.imgur.com/Dod0FE4.png"
+    COMMON_FISH   = "https://i.imgur.com/I3jU3p7.png"
+    RARE_FISH     = "https://i.imgur.com/7f90E9p.png"
+    TROPICAL_FISH = "https://i.imgur.com/ZBVZRXw.png"
+    SHARK         = "https://i.imgur.com/NMeTrjK.png"
+    GOLDEN_FISH   = "https://i.imgur.com/o2m9RkM.png"
+    PIG           = "https://i.imgur.com/v1Qa101.png"
+    DEER          = "https://i.imgur.com/fGl3N7s.png"
+    WOLF          = "https://i.imgur.com/k0fRgHl.png"
+    BEAR          = "https://i.imgur.com/4CryBHi.png"
+    LION          = "https://i.imgur.com/FBOyDvA.png"
+    COPPER_COIN   = "https://i.imgur.com/BR73e8Q.png"
+    ELEPHANT      = "https://i.imgur.com/qchItpk.png"
+    RUBY          = "https://i.imgur.com/rqOuL5x.png"
+    SAPPHIRE      = "https://i.imgur.com/9zrHNnb.png"
+    AMETHYST      = "https://i.imgur.com/cookhhr.png"
