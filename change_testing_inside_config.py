93d122ba8db75e7bf0e90961a03d279702915b1a
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-16 13:54:04 +0400

    added change_testing_inside_config.py

---
+++ change_testing_inside_config.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import toml
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+cfg = toml.load(os.path.join(PATH, "bobbie.toml"))
+cfg["bot"]["testing"] = "--test" in sys.argv
+
+print(cfg)
+
+with open(os.path.join(PATH, "bobbie.toml"), "w", encoding="utf-8") as f:
+    toml.dump(cfg, f)
