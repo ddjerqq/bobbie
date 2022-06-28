7a3b6c0eb27e06b388841b0a3f6d47a06a91c203
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-06-28 11:33:41 +0400

    made on_ready's user loading faster

--- cogs/events/on_ready.py
+++ cogs/events/on_ready.py
@@ -1,3 +1,5 @@
+import asyncio as aio
+import disnake
 from disnake.ext import commands
 from client.client import Client
 from database.factories.user_factory import UserFactory
@@ -6,24 +8,25 @@ from database.factories.user_factory import UserFactory
 class OnReady(commands.Cog):
     def __init__(self, client: Client):
         self.client = client
+        self.all_ids: set[int] = set()
+
+    async def add_or_update_new_users_in_a_guild(self, guild: disnake.Guild):
+        for member in filter(lambda u: not u.bot, guild.members):
+            if member.id not in self.all_ids:
+                user = UserFactory.new(member.id, member.name)
+                await self.client.db.users.add(user)
+                await self.client.logger.log(f"added {user}")
 
     async def add_or_update_new_users(self):
-        all_users = await self.client.db.users.get_all()
-        all_ids   = list(map(lambda u: u.id, all_users))
-
-        for guild in self.client.guilds:
-            for member in guild.members:
-                if member.bot:
-                    continue
-                if member.id not in all_ids:
-                    user = UserFactory.new(member.id, member.name)
-                    await self.client.db.users.add(user)
-                    await self.client.logger.log(f"added {user}")
-
+        group = [self.add_or_update_new_users_in_a_guild(guild) for guild in self.client.guilds]
+        await aio.gather(*group)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.client.logger.log("bobbi online")
+        await self.client.logger.log(f"logged in as {self.client.user}")
+        all_users = await self.client.db.users.get_all()
+        self.all_ids = set(map(lambda u: u.id, all_users))
+        await self.add_or_update_new_users()
 
 
 def setup(client: Client):
